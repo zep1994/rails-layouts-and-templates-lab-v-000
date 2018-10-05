7793d5ccbf82f940d156f05802f3f4d0f7991694
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-05 02:51:29 +0000

    automatically backed up by learn

--- spec/views/store_admin/invoice.html.erb_spec.rb
+++ spec/views/store_admin/invoice.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "store_admin/invoice.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe "store_admin/invoice.html.erb", type: :view do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
