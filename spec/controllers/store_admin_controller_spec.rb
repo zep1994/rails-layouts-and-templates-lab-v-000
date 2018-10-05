6f299bfae750e7de09915fa1c34f25d45cdd1af0
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-10-05 02:24:36 +0000

    automatically backed up by learn

--- spec/controllers/store_admin_controller_spec.rb
+++ spec/controllers/store_admin_controller_spec.rb
@@ -1,42 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe StoreAdminController, type: :controller do
-  include Capybara::DSL
   render_views
 
   describe "GET home" do
     it "renders the home template with admin layout" do
-      visit 'admin/home'
-
+      get :home
       expect(response).to render_template("home")
       expect(response).to render_template("layouts/admin")
 
-      expect(page.find("h1").text).to eq("Flatiron Widgets: Admin")
-      expect(page.find("h2").text).to eq("Welcome Flatiron Admin")
+      assert_select "h1", "Flatiron Widgets: Admin"
+      assert_select "h2", "Welcome Flatiron Admin"
     end
   end
 
   describe "GET orders" do
     it "renders the orders template with order_administration layout" do
-      visit 'admin/orders'
-
+      get :orders
       expect(response).to render_template("orders")
       expect(response).to render_template("layouts/order_administration")
 
-      expect(page.find("h1").text).to eq("Flatiron Widgets: Open Orders")
-      expect(page.find("h2").text).to eq("Welcome to Flatiron Open Orders")
-      expect(page.all("ol li").size).to be >= 1
+      assert_select "h1", "Flatiron Widgets: Open Orders"
+      assert_select "h2", "Welcome to Flatiron Open Orders"
+      assert_select "ol"
     end
   end
 
   describe "GET invoice" do
     it "renders the invoice template without any layout" do
-      visit 'admin/invoice'
-
+      get :invoice
       expect(response).to render_template("invoice")
       assert_template layout: false
 
-      expect(page.find("h1").text).to eq("Your Invoice")
+      assert_select "h1", "Your Invoice"
     end
   end
 end
